9d8ac7fe1ff737866ccd681f7c93c25ceb27a9b9
Dalila150 <dalilabaena@hotmail.com.ar>
2020-10-02 04:23:46 -0300

    Clase Conexion
    posee una ruta y 3 metodos para diferentes tipos de consultas

---
+++ TP5_GRUPO3/Clases/Conexion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Net;
+using System.Data;
+using System.Web.UI.WebControls;
+
+namespace TP5_GRUPO3.Clases
+{
+    public class Conexion
+    {
+
+        
+        string ruta = "Data Source="+ Dns.GetHostName() +"\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+        //string ruta = "Data Source=DESKTOP-AN768UP\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+
+        
+        public int ejecutarConsulta(string consulta) //Insertar, eliminar, modificar
+        {
+            SqlConnection conexion = new SqlConnection(ruta);
+            conexion.Open();
+
+            SqlCommand comando = new SqlCommand(consulta, conexion);
+            int filasAfectadas = comando.ExecuteNonQuery();
+
+            return filasAfectadas;
+        }
+
+
+        public SqlDataReader ejecutarConsultaReader(string consulta) //Select, devuelve un SqlDataReader
+        {
+            SqlConnection conexion = new SqlConnection(ruta);
+            conexion.Open();
+
+            SqlCommand comando = new SqlCommand(consulta, conexion);
+            SqlDataReader dr = comando.ExecuteReader();
+
+            return dr;
+        }
+
+        public SqlDataAdapter ejecutarConsultaAdapter(string consulta) //Select, devuelve un SqlDataAdapter
+        {
+            SqlConnection conexion = new SqlConnection(ruta);
+            conexion.Open();
+
+            SqlDataAdapter adap = new SqlDataAdapter(consulta, conexion);
+
+            return adap;
+
+        }
+
+
+
+    }
+
+}
--- TP5_GRUPO3/Ejercicio1.aspx.cs
+++ TP5_GRUPO3/Ejercicio1.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TP5_GRUPO3.Clases;
 
 namespace TP5_GRUPO3
 {
@@ -11,6 +14,19 @@ namespace TP5_GRUPO3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            string consulta = "select IdProducto, NombreProducto, CantidadPorUnidad, PrecioUnidad from Productos";
+            Conexion conectar = new Conexion();
+            
+            SqlDataAdapter adap = conectar.ejecutarConsultaAdapter(consulta);
+
+            DataSet ds = new DataSet();
+
+            adap.Fill(ds, "tablaProductos");
+            grdProductos.DataSource = ds.Tables["tablaProductos"];
+            grdProductos.DataBind();
+            
+
 
         }
     }
